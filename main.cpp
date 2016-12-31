39f9c02fb5a73008947eb017820d62e4a223c1d8
Brian Bosak <brielcccc@gmail.com>
2016-12-31 17:34:27 -0600

    Switched importing mechanism.

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 #include <list>
 #include <sstream>
 #include <map>
+#include "assimp/include/assimp/Importer.hpp"
+#include "assimp/include/assimp/postprocess.h"
+#include <assimp/scene.h>
 
 class Vector2 {
 public:
@@ -36,21 +39,11 @@ public:
 
 class GPUVertexDefinition {
 public:
-  Vector3 vert;
-  Vector2 texcoord;
-  Vector3 normal;
+  aiVector3D vert;
+  aiVector2D texcoord;
+  aiVector3D normal;
 };
 
-class Mesh {
-public:
-  std::vector<Vector3> verts;
-  std::vector<Vector2> texcoords;
-  std::vector<Vector3> normals;
-  std::list<GPUVertexDefinition> faces;
-  
-  //Number of vertices
-  size_t size = 0;
-};
 
 
 
@@ -66,174 +59,35 @@ int main(int argc, char** argv) {
     printf("Unable to open input file %s\n",argv[1]);
     return -1;
   }
-  int fd = open(argv[1],O_RDONLY);
-  char* mander = (char*)mmap(0,us.st_size,PROT_READ, MAP_SHARED, fd, 0);
+  Assimp::Importer importer;
+  auto scene = importer.ReadFile(argv[1],aiProcessPreset_TargetRealtime_MaxQuality);
   
-  char* ptr = mander;
-  //Parse OBJ file
-  auto expect = [&](const char* actors, char& found){
-    while(ptr<mander+us.st_size) {
-      for(size_t i = 0;actors[i] != 0;i++) {
-	if(*ptr == actors[i]) {
-	  found = *ptr;
-	  ptr++;
-	  return true;
-	}
-      }
-      ptr++;
-    }
-    return false;
-  };
-  auto readWhitespace = [&](){
-    while(ptr<mander+us.st_size && isspace(*ptr)){ptr++;}
-  };
   
-  auto readFloat = [&](){
-    std::stringstream ss;
-    while(ptr<mander+us.st_size && (isdigit(*ptr) || *ptr == '.' || *ptr == '-')) {ss<<*ptr;ptr++;}
-    return atof(ss.str().data());
-  };
-  auto readInt = [&](){
-    std::stringstream ss;
-    while(ptr<mander+us.st_size && isdigit(*ptr)) {ss<<*ptr;ptr++;}
-    return atoi(ss.str().data());
-  };
-  auto readIdentifier = [&](){
-    std::stringstream ss;
-    while(ptr<mander+us.st_size && !isspace(*ptr)){ss<<*ptr;ptr++;}
-    return ss.str();
-  };
-  auto readLine = [&](){
-    std::stringstream ss;
-    while(ptr<mander+us.st_size && *ptr != '\n'){ss<<*ptr;ptr++;}
-    return ss.str();
-  };
-  std::map<std::string,Mesh> meshes;
-  
-  std::string name = "root";
-  Mesh current;
-  
-  while(ptr<mander+us.st_size) {
-    readWhitespace();
-    char s = *ptr;
-    ptr++;
-    readWhitespace();
-  switch(s) {
-    case '#':
-      //Comment.
-    {
-      char found;
-      expect("\n",found);
-    }
-      break;
-    case 'v':
-    {
-      unsigned char n = *ptr;
-      
-      readWhitespace();
-      switch(n) {
-	case 't':
-	  //texture coordinate
-	{
-	    Vector2 texcoord;
-	    texcoord.x = readFloat();
-	    readWhitespace();
-	    texcoord.y = readFloat();
-	    current.texcoords.push_back(texcoord);
-	}
-	  break;
-	case 'n':
-	  //Normal
-	{
-	  Vector3 normal;
-	  normal.x = readFloat();
-	  readWhitespace();
-	  normal.y = readFloat();
-	  readWhitespace();
-	  normal.z = readFloat();
-	  current.normals.push_back(normal);
-	  
-	}
-	  break;
-	default:
-	  //Vertex
-	{
-	  
-	  Vector3 vertex;
-	  vertex.x = readFloat();
-	  readWhitespace();
-	  vertex.y = readFloat();
-	  readWhitespace();
-	  vertex.z = readFloat();
-	  
-	  current.verts.push_back(vertex);
-	  
-	}
-	  break;
-      }
-    }
-      break;
-	case 'o':
-	  meshes[name] = current;
-	  current = Mesh();
-	  name = readIdentifier();
-	  break;
-	case 'f':
-	{
-	  //Face
-	  while(*(ptr-1) != '\n' && ptr<mander+us.st_size) {
-	    
-	    GPUVertexDefinition def;
-	    int vertidx = readInt()-1;
-	    if(vertidx<0 || vertidx>=current.verts.size()) {
-	      printf("Error reading face attribute. Index %i is out of range of vertex buffer.\n",vertidx);
-	      return -1;
-	    }
-	    def.vert = current.verts[vertidx];
-	    ptr++;
-	    vertidx = readInt()-1;
-	    if(vertidx<0 || vertidx>=current.texcoords.size()) {
-	      printf("Error reading face attribute. Index %i is out of range of texcoord buffer.\n",vertidx);
-	      return -1;
-	    }
-	    def.texcoord = current.texcoords[vertidx];
-	    ptr++;
-	    vertidx = readInt()-1;
-	    if(vertidx<0 || vertidx>=current.normals.size()) {
-	      printf("Error reading face attribute. Index %i is out of range of normal buffer.\n",vertidx);
-	      return -1;
-	    }
-	    def.normal = current.normals[vertidx];
-	    ptr++;
-	    current.faces.push_back(def);
-	    current.size++;
-	  }
-	}
-	  break;
-	default:
-	  readLine();
-	  break;
-  }
-  }
-  
-  meshes[name] = current;
-  //Output stage:
-  munmap(mander,us.st_size);
-  close(fd);
-  fd = open(argv[2],O_CREAT | O_RDWR,S_IRUSR | S_IWUSR);
+  int fd = open(argv[2],O_CREAT | O_RDWR,S_IRUSR | S_IWUSR);
+  ftruncate(fd,0);
   unsigned char version = 0;
   write(fd,&version,1);
-  for(auto bot = meshes.begin();bot != meshes.end();bot++) {
-    uint16_t size = bot->first.size();
+  for(size_t i = 0;i<scene->mNumMeshes;i++) {
+    auto mesh = scene->mMeshes[i];
+    uint16_t size = mesh->mName.length;
     write(fd,&size,sizeof(size));
-    write(fd,bot->first.data(),size);
-    auto mobile = bot->second.faces.begin();
-    uint32_t usize = bot->second.size;
-    write(fd,&usize,sizeof(usize));
-    for(size_t i = 0;i<bot->second.size;i++) {
-      GPUVertexDefinition def = *mobile;
-      mobile++;
-      write(fd,&def,sizeof(def));
+    write(fd,mesh->mName.data,size);
+    size_t facecount = mesh->mNumFaces;
+    uint32_t numverts = facecount*3; //Since we've triangulated; each face has 3 vertices.
+    write(fd,&numverts,sizeof(numverts));
+    auto faces = mesh->mFaces;
+    auto verts = mesh->mVertices;
+    auto texcoords = mesh->mTextureCoords;
+    auto normals = mesh->mNormals;
+    
+    for(size_t c = 0;c<facecount;c++ /*This is how C++ was invented*/) {
+      for(size_t index = 0;index<3;index++) {
+	GPUVertexDefinition def;
+	def.vert = verts[faces[c].mIndices[index]];
+	def.texcoord = aiVector2D(texcoords[0][faces[c].mIndices[index]].x,texcoords[0][faces[c].mIndices[index]].y);
+	def.normal = normals[faces[c].mIndices[index]];
+	write(fd,&def,sizeof(def));
+      }
     }
   }
   close(fd);
